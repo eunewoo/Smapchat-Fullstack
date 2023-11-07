36b052f960c9d36daabe00be0718b8d8143cc3f7
ScalarVector <bobfishkins23@yahoo.com>
2023-11-07 09:37:48 -0500

    Response header logging

--- Backend/functions/middleware/logger.js
+++ Backend/functions/middleware/logger.js
@@ -1,7 +1,7 @@
 function logMiddleware(req, res, next) {
 
     console.log(`${req.method} request to ${req.originalUrl} with body:\n${req.body}\n`);
-    console.log(`response:\n${res}\n`);
+    console.log(`response headers:\n${res.getHeaders()}\n`);
     next();
 }
 
