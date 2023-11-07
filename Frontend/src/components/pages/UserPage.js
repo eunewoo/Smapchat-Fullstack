f4a7f9118b37b46de806b9315412628b219fb3da
Ulukbek Aitmatov <ulukbekaitmatov79@gmail.com>
2023-11-06 20:56:54 -0500

    connect to backend server

--- Frontend/src/components/pages/UserPage.js
+++ Frontend/src/components/pages/UserPage.js
@@ -3,35 +3,37 @@ import UserPopup from "../popups/UserPopup";
 
 export default function UserPage() {
 
-    // const [users, setUsers] = useState([]);
-    // var elems = [];
+    const [users, setUsers] = useState([]);
+    var elems = [];
 
-    // if (users.length <= 0)
-    // {
-    //     fetch(`${process.env.REACT_APP_URL}/Users`).then((res) => 
-    //     {
-    //         if (res.status === 200)
-    //         {
-    //             res.json().then((val) => 
-    //             {
-    //                 console.log(val);
-    //                 setUsers(val);
-    //             });
-    //         }
-    //         else
-    //         {
-    //             console.log("Error from server: " + res.status);
-    //         }
-    //     })
-    // }
-    // else
-    // {
-    //     users.forEach((id) => {
-    //         elems.push(<UserPopup ID={id._id}/>);
-    //     })
-    // }
+    if (users.length <= 0)
+    {
+        fetch(`${process.env.REACT_APP_URL}/Users`).then((res) => 
+        {
+            if (res.status === 200)
+            {
+                res.json().then((val) => 
+                {
+                    console.log(val);
+                    setUsers(val);
+                });
+            }
+            else
+            {
+                console.log("Error from server: " + res.status);
+            }
+        })
+    }
+    else
+    {
+        users.forEach((id) => {
+            elems.push(<UserPopup ID={id._id}/>);
+        })
+    }
 
     return (
-       <p>Hello World</p>
+        <>
+            {elems}
+        </>
     );
 }
