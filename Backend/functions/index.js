a79416af06e83f83aea4643bf25eb55b6fb4caaa
ScalarVector <bobfishkins23@yahoo.com>
2023-11-07 08:51:36 -0500

    Fix deploy

--- Backend/functions/index.js
+++ Backend/functions/index.js
@@ -10,5 +10,5 @@
 const functions = require("firebase-functions");
 const appPromise = require('./app.js'); 
 
-var app = await appPromise;
-exports.api = functions.https.onRequest(app);
+exports.api = functions.https.onRequest(appPromise);
+
