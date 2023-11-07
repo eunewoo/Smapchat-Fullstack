e1297db82428e238ca3c10dcebe5893bc9004dc9
bobfishkins23 <bobfishkins23@yahoo.com>
2023-11-07 01:54:50 -0500

    Functions directory test

--- Backend/middleware/logger.js
+++
@@ -1,7 +0,0 @@
-function logMiddleware(req, res, next) {
-
-    console.log(`${req.method} request to ${req.originalUrl} with body:\n${req.body}\n`);
-    next();
-}
-
-module.exports = logMiddleware;
