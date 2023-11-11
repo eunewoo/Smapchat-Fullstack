5f36319b03d04f78453cc74443b54fd85fcecfb7
eunewoo <87342625+eunewoo@users.noreply.github.com>
2023-11-11 17:32:03 -0500

    [Update]change map prop to useState again

--- Frontend/src/components/reuseable/MapRenderer.js
+++ Frontend/src/components/reuseable/MapRenderer.js
@@ -4,39 +4,29 @@
 /// JSON object for one of the applications map types in the
 /// GraphicData prop.
 // MapRender.js
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import { MapContainer, TileLayer, GeoJSON } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import { renderPictureMap } from './mapgraphics/PictureMap'; 
-
 import { renderArrowMap } from './mapgraphics/ArrowMap'; 
 import { renderBubbleMap } from './mapgraphics/BubbleMap'; 
 
 export default function MapRenderer({ Geometry, GraphicData, mapType }) {
-    const mapRef = useRef(null);
+    const [map, setMap] = useState(null);
 
     useEffect(() => {
-        console.log("mapRef", mapRef.current)
-        console.log("geometry", Geometry)
+        console.log("Map instance", map);
+        console.log("Geometry", Geometry);
 
-        if (mapType === 1 && mapRef.current) {
-            renderPictureMap(mapRef.current, GraphicData);
+        if (mapType === 1 && map) {
+            renderPictureMap(map, GraphicData);
+        } else if (mapType === 2 && map) {
+            renderArrowMap(map, GraphicData);
+        } else if (mapType === 3 && map) {
+            renderBubbleMap(map, GraphicData);
         }
-        else if (mapType === 2 && mapRef.current) {
-            
-            renderArrowMap(mapRef.current, GraphicData);
-        }
-        else if (mapType === 3 && mapRef.current) {
-            renderBubbleMap(mapRef.current, GraphicData);
-        }
-
-    //     else if (mapType === 4 && map) {
-    //         renderCategoryMap(map, GraphicData);
-    //     }
-    //     else if (mapType === 5 && map) {
-    //         renderScaleMap(map, GraphicData);
-    //     }
-    }, [Geometry, mapRef]);
+        // ... other map types
+    }, [map, Geometry, GraphicData, mapType]);
 
     return (
         <div className="map-rendering-box">
@@ -46,7 +36,7 @@ export default function MapRenderer({ Geometry, GraphicData, mapType }) {
                 center={[127.024, 37.532]}
                 minZoom={2}
                 maxBoundsViscosity={1}
-                whenCreated={ mapInstance => { this.mapRef.current = mapInstance }}
+                whenCreated={setMap}
             >
                 <TileLayer
                     noWrap={true}
@@ -60,6 +50,7 @@ export default function MapRenderer({ Geometry, GraphicData, mapType }) {
         </div>
     );
 }
+
 
 
 
