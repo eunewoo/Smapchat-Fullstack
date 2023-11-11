6d02c11974d72f42d033fb1ee741fffb4fd0f5fd
Ulukbek Aitmatov <ulukbekaitmatov79@gmail.com>
2023-11-11 04:10:38 -0500

    add HomePage skeleton

--- Frontend/src/components/Layout/Layout.js
+++ Frontend/src/components/Layout/Layout.js
@@ -1,9 +1,10 @@
 import { Outlet } from "react-router-dom";
 import Header from "../Header/Header";
+import "./Layout.css";
 
 const Layout = () => {
   return (
-    <div className="h-screen">
+    <div className="wholePage">
       <Header />
       <Outlet />
     </div>
--- Frontend/src/components/pages/Home/HomePage.js
+++ Frontend/src/components/pages/Home/HomePage.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
-  )
-}
+    <Container fluid>
+      <Row className="justify-content-center">
+        {/* remove className when you add maps */}
+        <Col md={8} className="bg-primary ">
+          Display Maps
+        </Col>
+        {/* remove className when you add display settings */}
 
-export default HomePage
+        <Col md={3} className="bg-secondary">
+          settings
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default HomePage;
