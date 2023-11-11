470d2642d1736b5668e60543fddad0c72ce5102a
Ulukbek Aitmatov <ulukbekaitmatov79@gmail.com>
2023-11-11 18:40:45 -0500

    add view map page

--- Frontend/src/routes/router.js
+++ Frontend/src/routes/router.js
@@ -8,6 +8,7 @@ import Layout from "../components/Layout/Layout";
 import CreatePage from "../components/pages/CreatePage/CreatePage";
 import MyMapsPage from "../components/pages/MyMapsPage/MyMapsPage";
 import HomePage from "../components/pages/Home/HomePage";
+import ViewMapPage from "../components/pages/ViewMapPage";
 
 
 export const router = createBrowserRouter(
@@ -16,6 +17,7 @@ export const router = createBrowserRouter(
       <Route index element={<HomePage />} />
       <Route path="create-page" element={<CreatePage />} />
       <Route path="my-maps-page" element={<MyMapsPage />} />
+      <Route path="view-map-page" element={<ViewMapPage/>}/>
     </Route>
   )
 );
