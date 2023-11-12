9362c8a45f27c9f8c13158fe7696fe588f0b9139
Ulukbek Aitmatov <ulukbekaitmatov79@gmail.com>
2023-11-11 22:14:10 -0500

    add AuthContext

--- Frontend/src/components/Header/Header.js
+++ Frontend/src/components/Header/Header.js
@@ -1,14 +1,20 @@
-import React, { useState } from "react";
-import { NavLink, useLocation } from "react-router-dom";
+import React, { useState, useContext } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
 import { Container, Navbar, Nav, Button } from "react-bootstrap";
 import Logo from "../../assets/images/Logo1.png";
 import UserAvatar from "../../assets/images/userAvatar.png";
+import AuthContext from "../../contexts/AuthContext";
 
 const Header = () => {
   const [menu, setMenu] = useState(false);
+  const { auth } = useContext(AuthContext);
 
-  const hideHeaderMenu = false;
+  const navigate = useNavigate();
 
+
+  const handleNavigateTo = () => navigate("/create-page");
+  
+  console.log(auth);
   return (
     <Navbar expand="lg" style={{ backgroundColor: "#0C0D34" }}>
       <Container>
@@ -27,6 +33,7 @@ const Header = () => {
           </Nav.Link>
         </Nav>
         <Button
+          onClick={handleNavigateTo}
           type="button"
           className="btn btn-primary px-4 py-2 rounded-3 fw-bold ms-3"
           style={{ backgroundColor: "#4ACEFF" }}
---
+++ Frontend/src/contexts/AuthContext.js
@@ -0,0 +1,69 @@
+import React, { createContext, useState, useContext, useCallback } from "react";
+
+export const AuthContext = createContext(null);
+
+export const AuthProvider = ({ children }) => {
+  const [auth, setAuth] = useState({ user: null, loggedIn: false });
+
+  const getLoggedIn = useCallback(async () => {
+  }, []);
+
+  const loginUser = useCallback(async (userData) => {
+  }, []);
+
+  const logoutUser = useCallback(async () => {
+  }, []);
+
+  const registerUser = useCallback(async (userData) => {
+  }, []);
+
+  const getUserName = useCallback(() => {
+  }, [auth]);
+
+  const resetPassword = useCallback(async () => {
+  }, []);
+
+  const updateUserName = useCallback(async () => {
+  }, []);
+
+  const updateEmail = useCallback(async () => {
+  }, []);
+
+  const updatePassword = useCallback(async () => {
+  }, []);
+
+  const deleteAccount = useCallback(async () => {
+  }, []);
+
+  const updateAvatar = useCallback(async () => {
+  }, []);
+
+  const updateToken = useCallback(async () => {
+  }, []);
+
+  // Auth context value that will be provided to components
+  const authContextValue = {
+    auth,
+    getLoggedIn,
+    loginUser,
+    logoutUser,
+    registerUser,
+    getUserName,
+    resetPassword,
+    updateUserName,
+    updateEmail,
+    updatePassword,
+    deleteAccount,
+    updateAvatar,
+    updateToken,
+  };
+
+  return (
+    <AuthContext.Provider value={authContextValue}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContext;
+
