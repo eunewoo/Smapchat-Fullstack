8d22a49bbe3c6d4da1e16007f7ac19fe7bfcbccb
eunewoo <87342625+eunewoo@users.noreply.github.com>
2023-11-13 17:57:28 -0500

    [Update]convert ArrowMap and SampleArrowMap to .json accepting form

--- Frontend/src/components/reuseable/mapgraphics/ArrowMap.js
+++ Frontend/src/components/reuseable/mapgraphics/ArrowMap.js
@@ -1,23 +1,25 @@
 // arrowMap.js
+
+
+// const sampleArrowMapData = [
+//     { name: "Gershwin Theater", latitude: 40.76242991263874, longitude: -73.98521056649075, order: 1, date: "2023-11-10"},
+//     { name: "Time Square", latitude: 40.75811079104577, longitude: -73.98534622655546, order: 2, date: "2023-11-11"},
+//     { name: "MoMA", latitude: 40.76157082004316, longitude: -73.9776430605575, order: 3, date: "2023-11-12"}
+// ];
+
+
 import L from 'leaflet';
-
-// three sample data of ArrowPointLocation format
-// later replace this part with real data from database
-const sampleArrowMapData = [
-    { name: "Gershwin Theater", latitude: 40.76242991263874, longitude: -73.98521056649075, order: 1, date: "2023-11-10"},
-    { name: "Time Square", latitude: 40.75811079104577, longitude: -73.98534622655546, order: 2, date: "2023-11-11"},
-    { name: "MoMA", latitude: 40.76157082004316, longitude: -73.9776430605575, order: 3, date: "2023-11-12"}
-];
-
+import sampleArrowMapJson from '../../editor/SampleArrowMap.json'; 
 
 export const renderArrowMap = (map) => {
-    const processedData = processArrowData(sampleArrowMapData);
+    const arrowMapData = convertJsonToArrowMapData(sampleArrowMapJson);
+    const processedData = processArrowData(arrowMapData);
 
     // Marker (pinpoint) drawing on map
     processedData.forEach(point => {
         // Create a marker with a label for the order number
         const marker = L.marker(point.position, {
-            // You can use a Leaflet DivIcon if you want to style the label further
+            // Use a Leaflet DivIcon to style the label
             icon: new L.DivIcon({
                 className: 'my-div-icon',
                 html: `<span>${point.order}</span>`
@@ -35,7 +37,16 @@ export const renderArrowMap = (map) => {
     }
 };
 
-// Order and organize data into format for used in leaflet to extract pinpoint & arrow
+const convertJsonToArrowMapData = (json) => {
+    return json.Location.map((loc, index) => ({
+        name: loc.Name,
+        latitude: loc.Lattitude,
+        longitude: loc.Longitude,
+        order: loc.Order,
+        date: loc.Date
+    }));
+};
+
 const processArrowData = (arrowMapData) => {
     return arrowMapData.sort((a, b) => a.order - b.order)
                        .map(point => ({
@@ -45,3 +56,4 @@ const processArrowData = (arrowMapData) => {
                            date: point.date
                        }));
 };
+
