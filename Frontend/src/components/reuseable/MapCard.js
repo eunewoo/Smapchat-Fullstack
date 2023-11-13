d5532ee728b64b84ccffa7956b40f55416b3c41d
Ulukbek Aitmatov <ulukbekaitmatov79@gmail.com>
2023-11-13 00:26:42 -0500

    connect scrollableGallary to MyMapsPage

--- Frontend/src/components/reuseable/MapCard.js
+++ Frontend/src/components/reuseable/MapCard.js
@@ -2,39 +2,41 @@ import { useContext, useState } from "react";
 import MapRenderer from "./MapRenderer";
 import RatingDisplay from "./RatingDisplay";
 import { navContext } from "../../App";
-import Card from 'react-bootstrap/Card';
-import './MapCard.css';
+import Card from "react-bootstrap/Card";
+import "./MapCard.css";
 
 /// Component which displays a preview and information about
 /// a map. Used for gallery pages to display maps to users.
 export default function MapCard(props) {
+  // The map data that this card is displaying
+  const [mapData, setMapData] = useState({});
 
-    // The map data that this card is displaying
-    const [mapData, setMapData] = useState({});
+  // Temporary hardcoded data for build 2!
+  if (Object.keys(mapData).length === 0) {
+    setMapData({
+      avgRate: 4,
+      title: "Cool map!",
+      author: "Alex",
+    });
+  }
 
-    // Temporary hardcoded data for build 2!
-    if (Object.keys(mapData).length === 0)
-    {
-        setMapData(
-            {
-                avgRate: 4,
-                title: "Cool map!",
-                author: "Alex"
-            }
-        )
-    }
-
-    // TODO: Add nav to display page once implemented
-    return(
-        <Card style={{width: "500px"}}>
-            <MapRenderer Geometry={mapData.mapFile} GraphicData={mapData} width="100%" height="300px"/>
-            <RatingDisplay value={mapData.avgRate}/>
-            <Card.Body style={{backgroundColor: "#141488", color: "white"}}>
-                <Card.Title>{mapData.title ?? "Loading..."}</Card.Title>
-                <Card.Text>
-                    by {mapData.author ?? "Loading..."}
-                </Card.Text>
-            </Card.Body>
-        </Card>
-    );
+  // TODO: Add nav to display page once implemented
+  return (
+    <Card className="m-3 " style={{ width: "30%" }}>
+      <MapRenderer
+        Geometry={mapData.mapFile}
+        GraphicData={mapData}
+        width="100%"
+        height="300px"
+      />
+      <RatingDisplay value={mapData.avgRate} />
+      <Card.Body
+        className=""
+        style={{ backgroundColor: "#141488", color: "white" }}
+      >
+        <Card.Title>{mapData.title ?? "Loading..."}</Card.Title>
+        <Card.Text>by {mapData.author ?? "Loading..."}</Card.Text>
+      </Card.Body>
+    </Card>
+  );
 }
--- Frontend/src/components/reuseable/ScrollableGallery.js
+++ Frontend/src/components/reuseable/ScrollableGallery.js
@@ -1,54 +1,69 @@
 import React, { useState } from "react";
 import MapCard from "./MapCard";
-import './ScrollableGallery.css';
+import "./ScrollableGallery.css";
 
 /// A scrollable container for MapCard components. Used for
 /// the gallery screens to allow users to scroll through many
 /// screens of possible maps to view.
 export default function ScrollableGallery(props) {
+  // Persist elements through re-render
+  const [elements] = useState([]);
+  // tracks which row needs be loaded next
+  // each row typically has 3 maps (may change?)
+  const [row, setRow] = useState(0);
 
-    // Persist elements through re-render
-    const [elements] = useState([]);
-    // tracks which row needs be loaded next
-    // each row typically has 3 maps (may change?)
-    const [row, setRow] = useState(0);
-
-    // TODO: Replace with actual get to fetch a row
-    if (elements.length <= 0) {
-        elements.push(
-            <div className="row">
-                <MapCard/>
-                <MapCard/>
-                <MapCard/>
-            </div>
-        );
-
-        setRow(row + 1);
-    }
-
-    // This handler handles the scrolling event, which will
-    // fetch a new set of maps and create map cards for them
-    // when the user is 90% of the way down the current scroll
-    const handleScroll = event => {
-        if (event.currentTarget.scrollTop >= event.currentTarget.scrollTopMax * 0.9) {
-            
-            console.log(`${event.currentTarget.scrollTop} exceeds ${event.currentTarget.scrollTopMax * 0.9} expanding list`);
-            // TODO: Replace with actual get to fetch a row
-            elements.push(
-                <div className="row">
-                    <MapCard/>
-                    <MapCard/>
-                    <MapCard/>
-                </div>
-            );
-
-            setRow(row + 1);
-        }
-    };
-
-    return (
-        <div className="scroller" onScroll={handleScroll}>
-            {elements}
-        </div>
+  // TODO: Replace with actual get to fetch a row
+  if (elements.length <= 0) {
+    elements.push(
+      <div className="row text-center">
+        <MapCard />
+        <MapCard />
+        <MapCard />
+        <MapCard />
+        <MapCard />
+        <MapCard />
+      </div>
     );
+
+    setRow(row + 1);
+  }
+
+  // This handler handles the scrolling event, which will
+  // fetch a new set of maps and create map cards for them
+  // when the user is 90% of the way down the current scroll
+  const handleScroll = (event) => {
+    if (
+      event.currentTarget.scrollTop >=
+      event.currentTarget.scrollTopMax * 0.9
+    ) {
+      console.log(
+        `${event.currentTarget.scrollTop} exceeds ${
+          event.currentTarget.scrollTopMax * 0.9
+        } expanding list`
+      );
+      // TODO: Replace with actual get to fetch a row
+      elements.push(
+        <div className="row">
+          <MapCard />
+          <MapCard />
+          <MapCard />
+          <MapCard />
+          <MapCard />
+          <MapCard />
+        </div>
+      );
+
+      setRow(row + 1);
+    }
+  };
+
+  return (
+    <div
+      className="scroller"
+      style={{ height: `calc(100vh - 170px)` }}
+      onScroll={handleScroll}
+    >
+      {elements}
+    </div>
+  );
 }
