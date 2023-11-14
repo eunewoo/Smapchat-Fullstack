679ef9ec8968076ca6c72e23725a321115374299
ScalarVector <bobfishkins23@yahoo.com>
2023-11-14 13:34:07 -0500

    Update test

--- Frontend/src/components/popups/UserPopup.cy.js
+++ Frontend/src/components/popups/UserPopup.cy.js
@@ -4,6 +4,6 @@ import UserPopup from "./UserPopup";
 describe("<UserPopup />", () => {
   it("renders", () => {
     // see: https://on.cypress.io/mounting-react
-    cy.mount(<UserPopup />);
+    cy.mount(<UserPopup user={{avatar:"", username:"username", email:"email" }}/>);
   });
 });
