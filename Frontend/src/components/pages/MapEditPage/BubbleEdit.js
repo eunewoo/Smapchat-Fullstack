215944557ba98fbf6d0ad0f5a1b728c5f6e14d16
Biniam <markosbiniam@gmail.com>
2023-11-16 18:49:58 -0500

    made change to the app.js to accept all requests from frontend

--- Frontend/src/components/pages/MapEditPage/BubbleEdit.js
+++ Frontend/src/components/pages/MapEditPage/BubbleEdit.js
@@ -2,7 +2,37 @@
 import { createBubbleMap } from "../../../util/mapUtil";
 
 export const BubbleSave = async () => {
-    
+    const exampleData = {
+      mapID: 55,
+      locationIds: [101, 102, 103],
+    };
+    const mapData = {
+      MapID: "1234TEST1234",
+      Location: [
+        {
+          Name: "Gershwin Theater",
+          Longitude: -73.9852,
+          Latitude: 40.7624,
+          Color: "#FF0000",
+          Size: 5,
+        },
+        {
+          Name: "Time Square",
+          Longitude: -73.9853,
+          Latitude: 40.7581,
+          Color: "#FFFF00",
+          Size: 1,
+        },
+        {
+          Name: "MoMA",
+          Longitude: -73.9776,
+          Latitude: 40.7615,
+          Color: "#FFFF00",
+          Size: 3,
+        },
+      ],
+    };
+    createBubbleMap(exampleData);
 };
 
 export const BubblePublish = async () => {};
