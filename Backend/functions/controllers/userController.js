fc016de2f797b8b37fac284e4648790f4066adc7
Ulukbek Aitmatov <ulukbekaitmatov79@gmail.com>
2023-11-16 21:05:35 -0500

    add login user to server

--- Backend/functions/controllers/userController.js
+++ Backend/functions/controllers/userController.js
@@ -1,4 +1,5 @@
 const UserModel = require("../database/model/UserModel");
+const bcrypt = require("bcryptjs");
 
 exports.getAllUsers = async (req, res, next) => {
   try {
@@ -39,6 +40,43 @@ exports.createUser = async (req, res, next) => {
       req.body.avatar
     );
     res.status(201).json(newUser);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+};
+
+exports.login = async (req, res, next) => {
+  try {
+    const user = await UserModel.findByEmail(req.body.email);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const isPasswordValid = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    // If email verification is required, you can check here if the user is verified
+    // if (!user.isVerified) {
+    //   return res.status(403).json({ message: "User not verified" });
+    // }
+
+    // Here we can create a token or session
+    // For example, using JWT (JSON Web Token):
+    // const token = jwt.sign({ id: user._id }, "your_jwt_secret", { expiresIn: "1h" });
+
+    // And then return the token or a success message
+    // res.status(200).json({ message: "Login successful", token });
+
+    const userWithoutPassword = { ...user._doc, password: undefined };
+
+    // Placeholder response for this example
+    res.status(200).json(userWithoutPassword);
   } catch (error) {
     console.error(error);
     next(error);
