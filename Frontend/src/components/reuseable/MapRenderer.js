8b193e03aa229e4694958a97d7fa65e8577d511f
eunewoo <87342625+eunewoo@users.noreply.github.com>
2023-11-18 11:47:27 -0500

    [Update]change ScaleMap's input data style to arrowmap for easy maintaining database

--- Frontend/src/components/reuseable/MapRenderer.js
+++ Frontend/src/components/reuseable/MapRenderer.js
@@ -1,6 +1,12 @@
 import Leaflet from "leaflet";
 import React, { useEffect, useRef, useState } from "react";
-import { MapContainer, TileLayer, GeoJSON, useMapEvent, useMapEvents } from "react-leaflet";
+import {
+  MapContainer,
+  TileLayer,
+  GeoJSON,
+  useMapEvent,
+  useMapEvents,
+} from "react-leaflet";
 import { renderPictureMap } from "./mapgraphics/PictureMap";
 import { renderArrowMap } from "./mapgraphics/ArrowMap";
 import { renderBubbleMap } from "./mapgraphics/BubbleMap";
@@ -9,48 +15,59 @@ import { renderScaleMap } from "./mapgraphics/ScaleMap";
 import "leaflet/dist/leaflet.css";
 import "./MapRenderer.css";
 
-/// Component which handles rendering a map in the application.
-/// Takes a valid GeoJSON object in the Geometry prop, and a custom
-/// JSON object for one of the applications map types in the
-/// GraphicData prop.
 export default function MapRenderer(props) {
   const mapRef = useRef(null);
   const [zoom, setZoom] = useState(2);
   const [layerGroup] = useState(Leaflet.layerGroup());
+  const [boundaries, setBoundaries] = useState([]); // State to store GeoJSON boundaries
 
   useEffect(() => {
     if (mapRef.current) {
       layerGroup.addTo(mapRef.current);
     }
-  });
+  }, [layerGroup]);
 
   layerGroup.eachLayer((layer) => layer.remove());
 
-  if (props.Geometry && mapRef.current) {
-    const geoJsonLayer = Leaflet.geoJSON(props.Geometry.features);
-    geoJsonLayer.addTo(layerGroup);
-    mapRef.current.on("zoomend", () => setZoom(mapRef.current.getZoom()));
-  }
+  useEffect(() => {
+    if (props.Geometry && mapRef.current) {
+      const geoJsonLayer = Leaflet.geoJSON(props.Geometry.features, {
+        onEachFeature: (feature, layer) => {
+          // Store each boundary in the array
+          setBoundaries((current) => [
+            ...current,
+            { feature: feature, layer: layer },
+          ]);
+        },
+      });
+      geoJsonLayer.addTo(layerGroup);
+      mapRef.current.on("zoomend", () => setZoom(mapRef.current.getZoom()));
+    }
+  }, [props.Geometry, mapRef, layerGroup]);
 
-  if (props.mapType === "PictureMap" && mapRef.current) {
-    renderPictureMap(layerGroup, props.GeoJsonData);
-  } else if (props.mapType === "ArrowMap" && mapRef.current) {
-    renderArrowMap(layerGroup, props.GeoJsonData);
-  } else if (props.mapType === "BubbleMap" && mapRef.current) {
-    renderBubbleMap(layerGroup, props.GeoJsonData);
-  } else if (
-    props.mapType === "CategoryMap" &&
-    mapRef.current &&
-    props.GeoJsonData
-  ) {
-    renderCategoryMap(layerGroup, props.GeoJsonData); // Pass the GeoJSON data
-  } else if (
-    props.mapType === "ScaleMap" &&
-    mapRef.current &&
-    props.GeoJsonData
-  ) {
-    renderScaleMap(layerGroup, props.GeoJsonData); // Pass the GeoJSON data
-  }
+  // Render maps based on the type
+  useEffect(() => {
+    if (props.mapType === "PictureMap" && mapRef.current) {
+      renderPictureMap(layerGroup, props.GeoJsonData);
+    } else if (props.mapType === "ArrowMap" && mapRef.current) {
+      renderArrowMap(layerGroup, props.GeoJsonData);
+    } else if (props.mapType === "BubbleMap" && mapRef.current) {
+      renderBubbleMap(layerGroup, props.GeoJsonData);
+    } else if (
+      props.mapType === "CategoryMap" &&
+      mapRef.current &&
+      props.GeoJsonData
+    ) {
+      renderCategoryMap(layerGroup, props.GeoJsonData, boundaries); // Pass the GeoJSON data
+    } else if (
+      props.mapType === "ScaleMap" &&
+      mapRef.current &&
+      props.GeoJsonData
+    ) {
+      renderScaleMap(layerGroup, props.GeoJsonData, boundaries); // Pass boundaries to ScaleMap
+    }
+    // [Other map types rendering code...]
+  }, [props, boundaries, layerGroup]);
 
   return (
     <div style={{ width: props.width, height: props.height }}>
@@ -62,7 +79,7 @@ export default function MapRenderer(props) {
         maxBoundsViscosity={1}
         ref={mapRef}
       >
-        <ClickHandler onClick={props.onClick}/>
+        <ClickHandler onClick={props.onClick} />
         <TileLayer
           noWrap={true}
           url="http://a.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png"
@@ -75,13 +92,13 @@ export default function MapRenderer(props) {
 }
 
 function ClickHandler(props) {
-    useMapEvents({
-        click(e){
-            if(props.onClick != null) {
-                props.onClick(e.latlng);
-            }
-        }
-      })
+  useMapEvents({
+    click(e) {
+      if (props.onClick != null) {
+        props.onClick(e.latlng);
+      }
+    },
+  });
 
-    return(<></>);
+  return <></>;
 }
--- Frontend/src/components/reuseable/mapgraphics/ScaleMap.js
+++ Frontend/src/components/reuseable/mapgraphics/ScaleMap.js
@@ -1,19 +1,19 @@
 import L from "leaflet";
+// import { boundaries } from "./MapRenderer.js"; // Import boundaries from MapRenderer.js
 
 // Function to blend two colors based on a percentage
 const blendColors = (color1, color2, percentage) => {
-  // Assuming color1 and color2 are hex colors (e.g., #FF0000)
   let r = Math.round(
     parseInt(color1.substring(1, 3), 16) * (1 - percentage) +
-      parseInt(color2.substring(1, 3), 16) * percentage,
+      parseInt(color2.substring(1, 3), 16) * percentage
   );
   let g = Math.round(
     parseInt(color1.substring(3, 5), 16) * (1 - percentage) +
-      parseInt(color2.substring(3, 5), 16) * percentage,
+      parseInt(color2.substring(3, 5), 16) * percentage
   );
   let b = Math.round(
     parseInt(color1.substring(5, 7), 16) * (1 - percentage) +
-      parseInt(color2.substring(5, 7), 16) * percentage,
+      parseInt(color2.substring(5, 7), 16) * percentage
   );
 
   return `#${r.toString(16).padStart(2, "0")}${g
@@ -21,52 +21,66 @@ const blendColors = (color1, color2, percentage) => {
     .padStart(2, "0")}${b.toString(16).padStart(2, "0")}`;
 };
 
-// Sample Category data - initially empty
-const categories = [];
+// Assuming blendColors function is already defined here
 
-const createCategoriesFromGeoJson = (geoJsonData) => {
-
-  const max = geoJsonData.Location.reduce((a, b) => a.Value > b.Value ? a : b).Value;
-
-  geoJsonData.Location.forEach((feature, index) => {
-    const scalePercentage = feature.Value / max; // Calculate the percentage (0 to 1)
-    const categoryColor = blendColors(
-      geoJsonData.MinColor,
-      geoJsonData.MaxColor,
-      scalePercentage,
-    );
-
-    const newCategory = {
-      CategoryId: index + 1,
-      Name: `Category ${index + 1}`,
-      Color: categoryColor,
-      Coordinates: feature.Polygon.Coordinates,
-    };
-    categories.push(newCategory);
-    console.log(`Created category: ${newCategory.Name}`, newCategory);
-  });
-
-  console.log("All created categories:", categories);
+// Function to color a specific boundary based on lat, lng
+const colorBoundary = (lat, lng, color, boundaries) => {
+  const boundary = boundaries.find((b) =>
+    b.layer.getBounds().contains([lat, lng])
+  );
+  if (boundary) {
+    boundary.layer.setStyle({
+      fillColor: color,
+      fillOpacity: 0.5,
+      weight: 2,
+    });
+  }
 };
 
-export const renderScaleMap = (map, geoJsonData) => {
-  if (!geoJsonData) {
+// Function to create categories from data
+const createCategoriesFromData = (data) => {
+  const max = data.Location.reduce((a, b) => (a.Value > b.Value ? a : b)).Value;
+  return data.Location.map((location, index) => {
+    const scalePercentage = location.Value / max;
+    const categoryColor = blendColors(
+      data.MinColor,
+      data.MaxColor,
+      scalePercentage
+    );
+
+    return {
+      CategoryId: index + 1,
+      Name: location.Name,
+      Color: categoryColor,
+      Position: [location.Lattitude, location.Longitude],
+    };
+  });
+};
+
+// Main function to render the scale map
+export const renderScaleMap = (map, data, boundaries) => {
+  if (!data) {
     return;
   }
 
-  createCategoriesFromGeoJson(geoJsonData);
+  const categories = createCategoriesFromData(data);
 
   categories.forEach((category) => {
-    const leafletCoordinates = category.Coordinates.map((coord) => [
-      coord[1],
-      coord[0],
-    ]);
+    colorBoundary(
+      category.Position[0],
+      category.Position[1],
+      category.Color,
+      boundaries
+    );
 
-    L.polygon(leafletCoordinates, {
+    // Add circle marker (if needed)
+    L.circleMarker(category.Position, {
       color: category.Color,
       fillColor: category.Color,
       fillOpacity: 0.5,
-      weight: 2,
-    }).addTo(map);
+      radius: 10,
+    })
+      .addTo(map)
+      .bindPopup(`${category.Name}: ${category.Color}`);
   });
 };
