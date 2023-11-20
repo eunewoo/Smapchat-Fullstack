2654ecd8d02e68fc754b355342b7736500502464
eunewoo <87342625+eunewoo@users.noreply.github.com>
2023-11-20 18:33:16 -0500

    [Add]add comments on CategoryEdit.js and indicate completed endpoints in there

--- Frontend/src/components/pages/MapEditPage/CategoryEdit.js
+++ Frontend/src/components/pages/MapEditPage/CategoryEdit.js
@@ -1,31 +1,27 @@
 //importing routers from map Util
 import {
   createCategoryMap,
-  fetchSpecificMap,
   getCategoryMap,
   updateMap,
   updateMapStatus,
 } from "../../../util/mapUtil";
 
+// Sample data that will be replaced by global context later
 const exampleMap = {
   MapID: 253,
   Category: [
     {
-      Name: "Red places",
+      Name: "Red places2",
       Locations: [
         {
           Longitude: -73.9852,
           Lattitude: 40.7624,
         },
-        {
-          Longitude: -74.1254,
-          Lattitude: 40.59206,
-        },
       ],
       Color: "#222222",
     },
     {
-      Name: "Blue places",
+      Name: "Blue places2",
       Locations: [
         {
           Longitude: -73.9654,
@@ -62,17 +58,21 @@ const exampleUser = {
 const exampleUserId = "6556b9cde82b7d9bd50261ff";
 
 //1 for getting Category map info
+// COMPLETE
 export const fetchCategoryMap = async () => {
   const map = await getCategoryMap(mapInfo.MapID);
   return map;
 };
 
 //2 for creating or updating if one excits Category map info
+// create: COMPLETE with clicking Save button
+// update: COMPLETE with clicking Save button
 export const CategorySave = async () => {
   createCategoryMap(exampleUserId, exampleUser, exampleMap, mapInfo);
 };
 
 //3 changing the map publish status
+//
 export const CategoryPublish = async (examplePublicStatus) => {
   updateMapStatus(exampleUserId, exampleMap.MapID, examplePublicStatus);
 };
@@ -81,8 +81,10 @@ export const CategoryPublish = async (examplePublicStatus) => {
 export const updateSpecificMap = async () => {
   updateMap(exampleUserId, mapInfo.MapID, mapInfo);
 };
-//5 getting any map
-export const fetchMap = async () => {
-  const map = await fetchSpecificMap(exampleMap.MapID);
-  return map;
-};
+
+// //5 getting any map
+// // Not necessary
+// export const fetchMap = async () => {
+//   const map = await fetchSpecificMap(exampleMap.MapID);
+//   return map;
+// };
