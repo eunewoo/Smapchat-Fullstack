d267fb8d84b12ef2442b15a2aad7b55ae437c59c
Biniam <markosbiniam@gmail.com>
2023-11-19 22:45:20 -0500

    [fix error] handled error from when fetched map is empty

--- Frontend/src/components/pages/MapEditPage/BubbleEdit.js
+++ Frontend/src/components/pages/MapEditPage/BubbleEdit.js
@@ -11,21 +11,21 @@ const exampleMap = {
   MapID: 3,
   Location: [
     {
-      Name: "Gershwin Theater",
+      Name: "Seoul",
       Longitude: -73.9852,
       Lattitude: 40.7624,
       Color: "#FF0000",
       Size: 5,
     },
     {
-      Name: "Time Square",
+      Name: "Busan",
       Longitude: -73.9853,
       Lattitude: 40.7581,
       Color: "#FFFF00",
       Size: 1,
     },
     {
-      Name: "MoMA",
+      Name: "Jeju",
       Longitude: -73.9776,
       Lattitude: 40.7615,
       Color: "#FFFF00",
