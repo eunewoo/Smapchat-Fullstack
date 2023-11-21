d56c00c2e7077a5d261e6678d0feb27cb2290a44
Ulukbek Aitmatov <ulukbekaitmatov79@gmail.com>
2023-11-20 21:20:17 -0500

    fix picture map schema

--- Backend/functions/database/schema/PictureMap.js
+++ Backend/functions/database/schema/PictureMap.js
@@ -1,17 +1,56 @@
 const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
-const PictureMapSchema_ = new mongoose.Schema({
-  mapID: {
-    type: Number,
+const LibrarySchema = new Schema({
+  Name: {
+    type: String,
     required: true,
-    unique: true,
+    unique: false,
   },
-  locationIds: {
-    type: [Number],
+  Images: {
+    type: [String],
     required: false,
     unique: false,
   },
 });
 
-const PictureSchema = mongoose.model("PictureMap", PictureMapSchema_);
-module.exports = PictureSchema;
+const PictureMapLocationSchema = new Schema({
+  Name: {
+    type: String,
+    required: true,
+    unique: false,
+  },
+  Longitude: {
+    type: String,
+    required: true,
+    unique: false,
+  },
+  Lattitude: {
+    type: String,
+    required: true,
+    unique: false,
+  },
+  Library: [LibrarySchema],
+});
+
+const PictureMapSchema = new Schema({
+  MapID: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  Location: [PictureMapLocationSchema],
+});
+
+const LibraryModel = mongoose.model("Library", LibrarySchema);
+const PictureMapLocationModel = mongoose.model(
+  "PictureMapLocation",
+  PictureMapLocationSchema
+);
+const PictureMapModel = mongoose.model("PictureMap", PictureMapSchema);
+
+module.exports = {
+  LibraryModel,
+  PictureMapLocationModel,
+  PictureMapModel,
+};
