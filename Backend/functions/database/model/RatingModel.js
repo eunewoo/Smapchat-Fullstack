99b566e18c0333b1bd300f4a45e8cee57965104d
eunewoo <87342625+eunewoo@users.noreply.github.com>
2023-11-27 15:16:21 -0500

    [Update]make delete rating now contains updating

--- Backend/functions/database/model/RatingModel.js
+++ Backend/functions/database/model/RatingModel.js
@@ -64,10 +64,33 @@ class RatingModel {
   //delete
   static async deleteRate(userId) {
     try {
-      const deletedRates = await RatingSchema.deleteMany({
-        userID: userId,
-      });
-      return deletedRates;
+      // Find all ratings to be deleted to get their mapIDs
+      const ratingsToDelete = await RatingSchema.find({ userID: userId });
+      const mapIds = ratingsToDelete.map((rating) => rating.mapID);
+
+      // Delete the ratings
+      await RatingSchema.deleteMany({ userID: userId });
+
+      // Update avgRate for each affected map
+      for (const mapId of mapIds) {
+        const remainingRatings = await RatingSchema.find({ mapID: mapId });
+        let avgRate = 0;
+
+        if (remainingRatings.length > 0) {
+          const totalRate = remainingRatings.reduce(
+            (acc, curr) => acc + curr.rate,
+            0
+          );
+          avgRate = totalRate / remainingRatings.length;
+        }
+
+        await MapSchema.findByIdAndUpdate(mapId, { avgRate: avgRate });
+      }
+
+      return {
+        success: true,
+        message: "Ratings deleted and average rates updated",
+      };
     } catch (error) {
       throw new Error(error.message);
     }
