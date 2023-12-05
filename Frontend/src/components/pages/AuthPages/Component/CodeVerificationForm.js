b0de36779babd61eff6d5ce2dd90596759205235
Ulukbek Aitmatov <ulukbekaitmatov79@gmail.com>
2023-12-04 23:35:53 -0500

    update recovery password UI to code based authentication

---
+++ Frontend/src/components/pages/AuthPages/Component/CodeVerificationForm.js
@@ -0,0 +1,111 @@
+import React, { useState, useContext } from "react";
+import { Form, Button, Spinner, Alert } from "react-bootstrap";
+import { AuthContext } from "../../../../contexts/AuthContext";
+
+const CodeVerificationForm = () => {
+  const { isLoading } = useContext(AuthContext);
+  const [code, setCode] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [isCodeVerified, setIsCodeVerified] = useState(false);
+  const [error, setError] = useState("");
+
+  const handleVerifyCode = (e) => {
+    e.preventDefault();
+
+    console.log(code);
+  };
+
+  const handlePasswordReset = (e) => {
+    e.preventDefault();
+    if (newPassword !== confirmPassword) {
+      setError("Passwords do not match. Please try again.");
+      return;
+    }
+    if (newPassword.length < 6) {
+      setError("Password should be at least 6 characters.");
+      return;
+    }
+    setError("");
+    console.log("New Password:", newPassword);
+    // Add logic to reset the password
+  };
+
+  return (
+    <>
+      {!isCodeVerified ? (
+        <>
+          <div className="mb-4">
+            Enter the code that we sent to your email. If you don't see the
+            email in your inbox, please check your spam folder or wait a few
+            more minutes, as it might take some time to arrive.
+          </div>
+          <Form
+            className="login-form"
+            onSubmit={handleVerifyCode}
+            style={{ maxWidth: "40%" }}
+          >
+            <Form.Group className="mb-3" controlId="formBasicVerificationCode">
+              <Form.Control
+                type="text"
+                placeholder="Enter Code"
+                value={code}
+                onChange={(e) => setCode(e.target.value)}
+              />
+            </Form.Group>
+            <Button className="btn btn-2 mx-auto mb-5" size="lg" type="submit">
+              {isLoading ? (
+                <>
+                  <Spinner
+                    as="span"
+                    animation="border"
+                    size="sm"
+                    role="status"
+                    aria-hidden="true"
+                  />
+                  <span className="ms-2">Loading...</span>
+                </>
+              ) : (
+                "Verify Code"
+              )}
+            </Button>
+          </Form>
+        </>
+      ) : (
+        <>
+          <div className="mb-4">Set your new password.</div>
+          <Form
+            className="login-form"
+            onSubmit={handlePasswordReset}
+            style={{ maxWidth: "40%" }}
+          >
+            <Form.Group className="mb-3" controlId="formBasicNewPassword">
+              <Form.Control
+                type="password"
+                placeholder="New Password"
+                value={newPassword}
+                onChange={(e) => setNewPassword(e.target.value)}
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicConfirmPassword">
+              <Form.Control
+                type="password"
+                placeholder="Confirm Password"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
+            </Form.Group>
+            {error && <Alert variant="danger">{error}</Alert>}
+
+            <Button className="btn btn-2 mx-auto mb-5" size="lg" type="submit">
+              Reset Password
+            </Button>
+          </Form>
+        </>
+      )}
+    </>
+  );
+};
+
+export default CodeVerificationForm;
--- Frontend/src/components/pages/AuthPages/PasswordRecoveryPage.js
+++ Frontend/src/components/pages/AuthPages/PasswordRecoveryPage.js
@@ -10,28 +10,32 @@ import {
 } from "react-bootstrap";
 import Logo from "../../../assets/images/logo2.png";
 import { AuthContext } from "../../../contexts/AuthContext";
+import CodeVerificationForm from "./Component/CodeVerificationForm";
 
 const PasswordRecoveryPage = () => {
   const { resetPassword, isLoading } = useContext(AuthContext);
   const [email, setEmail] = useState("");
   const [feedback, setFeedback] = useState("");
-
-  // const handleRouteToHome = () => navigate("/");
+  const [verified, setVerified] = useState(false);
 
   const handleResetPassword = async (e) => {
     e.preventDefault();
     const { success, error } = await resetPassword(email);
     if (success) {
       setFeedback(
-        "Instructions to reset your password have been sent to your email."
+        `Instructions to reset your password have been sent to your email: ${email}.`
       );
+      setVerified(true);
       // Optionally navigate to a different page or show a message
     } else {
       setFeedback(
         error || "An error occurred while trying to reset the password."
       );
+      setVerified(false);
     }
   };
+
+  
 
   return (
     <Container className="d-flex vh-100">
@@ -61,42 +65,52 @@ const PasswordRecoveryPage = () => {
               </Card.Title>
             </Card.Body>
           </Card>
-          <div className="mb-4">
-            Forgot your password? Don’t worry, it happens to the best of us.
-            Just enter your registered email address below, and we'll send you
-            instructions on how to reset it.
-          </div>
-          <Form
-            className="login-form"
-            onSubmit={handleResetPassword}
-            style={{ maxWidth: "40%" }}
-          >
-            <Form.Group className="mb-3" controlId="formBasicRecoveryEmail">
-              <Form.Control
-                type="email"
-                placeholder="Recovery E-Mail"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </Form.Group>
-
-            <Button className="btn btn-2 mx-auto mb-5" size="lg" type="submit">
-              {isLoading ? (
-                <>
-                  <Spinner
-                    as="span"
-                    animation="border"
-                    size="sm"
-                    role="status"
-                    aria-hidden="true"
+          {!verified ? (
+            <>
+              <div className="mb-4">
+                Forgot your password? Don’t worry, it happens to the best of us.
+                Just enter your registered email address below, and we'll send
+                you instructions on how to reset it.
+              </div>
+              <Form
+                className="login-form"
+                onSubmit={handleResetPassword}
+                style={{ maxWidth: "40%" }}
+              >
+                <Form.Group className="mb-3" controlId="formBasicRecoveryEmail">
+                  <Form.Control
+                    type="email"
+                    placeholder="Recovery E-Mail"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
                   />
-                  <span className="ms-2">Loading...</span>
-                </>
-              ) : (
-                "Send Reset Instructions"
-              )}
-            </Button>
-          </Form>
+                </Form.Group>
+
+                <Button
+                  className="btn btn-2 mx-auto mb-5"
+                  size="lg"
+                  type="submit"
+                >
+                  {isLoading ? (
+                    <>
+                      <Spinner
+                        as="span"
+                        animation="border"
+                        size="sm"
+                        role="status"
+                        aria-hidden="true"
+                      />
+                      <span className="ms-2">Loading...</span>
+                    </>
+                  ) : (
+                    "Send Reset Instructions"
+                  )}
+                </Button>
+              </Form>
+            </>
+          ) : (
+            <CodeVerificationForm />
+          )}
           {feedback && <div className="feedback-message">{feedback}</div>}
         </Col>
       </Row>
