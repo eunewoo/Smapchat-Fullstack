a829c93453746d7e47d2860b1ebb4e33d9af1292
eunewoo <87342625+eunewoo@users.noreply.github.com>
2023-12-17 01:23:59 -0500

    [Update]Arrowmap boldness animation effect and order font size increase2

--- Frontend/src/components/reuseable/mapgraphics/ArrowMap.js
+++ Frontend/src/components/reuseable/mapgraphics/ArrowMap.js
@@ -6,7 +6,6 @@ export const renderArrowMap = (map, data) => {
   }
 
   const arrowMapData = convertJsonToArrowMapData(data);
-  console.log(arrowMapData);
   const processedData = processArrowData(arrowMapData);
 
   // Marker (pinpoint) drawing on map
@@ -16,7 +15,7 @@ export const renderArrowMap = (map, data) => {
       // Use a Leaflet DivIcon to style the label
       icon: new L.DivIcon({
         className: "my-div-icon",
-        html: `<span>${point.order}</span>`,
+        html: `<span style="font-size: 20px; font-weight: bold;">${point.order}</span>`, // Increased font size and made it bold
       }),
     }).addTo(map);
 
@@ -24,14 +23,28 @@ export const renderArrowMap = (map, data) => {
     marker.bindPopup(`<b>${point.name}</b><br>Date: ${point.date}`);
   });
 
-  // Draw arrows (polylines) between points
-  if (processedData.length > 1) {
-    const polyline = L.polyline(
-      processedData.map((point) => point.position),
-      { color: "blue" }
-    );
-    polyline.addTo(map);
-  }
+  // Draw the original arrow (polyline)
+  const polyline = L.polyline(
+    processedData.map((point) => point.position),
+    { color: "blue" }
+  ).addTo(map);
+
+  // Function to animate each segment in sequence
+  const animateNextSegment = (index) => {
+    if (index < processedData.length - 1) {
+      animateSegment(
+        map,
+        processedData[index].position,
+        processedData[index + 1].position,
+        () => {
+          animateNextSegment(index + 1);
+        }
+      );
+    }
+  };
+
+  // Start animating from the first segment
+  animateNextSegment(0);
 };
 
 const convertJsonToArrowMapData = (json) => {
@@ -53,4 +66,39 @@ const processArrowData = (arrowMapData) => {
       order: point.order,
       date: point.date,
     }));
+};
+
+// This function deals with animating sequential boldness of arrow
+const animateSegment = (map, start, end, onComplete) => {
+  let currentWeight = 5; // Initial weight of the line
+  const maxWeight = 10; // Max weight when the line is "bold"
+  const increaseRate = 0.1; // Rate of weight increase
+  const decreaseRate = 0.05; // Rate of weight decrease
+  let increasing = true;
+
+  const segment = L.polyline([start, end], {
+    color: "blue",
+    weight: currentWeight,
+  }).addTo(map);
+
+  const animate = () => {
+    if (increasing) {
+      currentWeight += increaseRate;
+      if (currentWeight >= maxWeight) {
+        increasing = false;
+      }
+    } else {
+      currentWeight -= decreaseRate;
+      if (currentWeight <= 5) {
+        map.removeLayer(segment);
+        onComplete();
+        return;
+      }
+    }
+
+    segment.setStyle({ weight: currentWeight });
+    requestAnimationFrame(animate);
+  };
+
+  requestAnimationFrame(animate);
 };
