ce35e116bdd8bd697b15ba17d2e00cff65764df2
bobfishkins23 <bobfishkins23@yahoo.com>
2023-12-17 10:37:45 -0500

    Revert mymaps change as header link already deflects users

--- Frontend/src/components/pages/MyMapsPage/MyMapsPage.js
+++ Frontend/src/components/pages/MyMapsPage/MyMapsPage.js
@@ -1,19 +1,14 @@
-import React, { useContext } from "react";
+import React from "react";
 import { useState } from "react";
 import "./MyMaps.css";
 import ScrollableGallery from "../../reuseable/ScrollableGallery";
 import SearchWidget from "../../reuseable/SearchWidget";
-import AuthContext from "../../../contexts/AuthContext";
 
 import { fetchUserMaps, fetchUserSearchMaps } from "../../../util/mapUtil";
-import { useNavigate, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 const MyMapsPage = () => {
-  const { auth } = useContext(AuthContext);
-  const navigate = useNavigate();
   var params = useParams();
-
-  const isLoggedIn = auth.loggedIn;
 
   const fetchData = async (page, limit) => {
     try {
@@ -49,12 +44,8 @@ const MyMapsPage = () => {
     }
   };
 
-  const handleNavigateToLogin = () => navigate("/login-page");
-
   return (
     <div className="container-fluid mt-4">
-      {/* remove height and color from the css when you add components */}
-      {isLoggedIn ? (
         <>
           <div className="row justify-content-center">
             <div className="left">
@@ -71,14 +62,6 @@ const MyMapsPage = () => {
             </div>
           </div>
         </>
-      ) : (
-        <div className="text-center mt-5">
-          <p>Please log in to access the My Maps feature.</p>
-          <button onClick={handleNavigateToLogin} className="btn btn-primary">
-            Go to Login
-          </button>
-        </div>
-      )}
     </div>
   );
 };
