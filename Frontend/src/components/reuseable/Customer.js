46b515d70f379dd8e7bce721dd561a58bf76e4fb
Biniam <markosbiniam@gmail.com>
2023-12-17 01:52:10 -0500

    [Fix] bug fixed in changing status

--- Frontend/src/components/reuseable/Customer.js
+++ Frontend/src/components/reuseable/Customer.js
@@ -1,17 +1,26 @@
-import { useContext, useState } from "react";
-import { Image, Card, Container, Button, Dropdown } from "react-bootstrap";
+import React, { useContext, useState } from "react";
+import {
+  Image,
+  Card,
+  Container,
+  Button,
+  Dropdown,
+  Spinner,
+} from "react-bootstrap";
 import { popContext } from "../../App";
 import "./Customer.css";
 import UserPopup from "../popups/UserPopup";
 import defaultAvatar from "../../assets/images/avatar.png";
-import { deleteUser, updateActivationStatus } from "../../util/userUtil";
+import { deleteUser, updateUserProfile } from "../../util/userUtil";
 import ConfirmationDialog from "../popups/ConfirmationDialog";
+import { useNavigate } from "react-router-dom";
 
 export default function Customer(props) {
-
   const setPop = useContext(popContext);
   const [isBanned, setIsBanned] = useState(props.userData.isActive);
   const [showConfirmation, setShowConfirmation] = useState(false);
+  const [admin, setAdmin] = useState(props.userData.userType === 1);
+  const [loading, setLoading] = useState(false);
 
   const handleDelete = () => {
     setShowConfirmation(true);
@@ -28,19 +37,37 @@ export default function Customer(props) {
   };
 
   const handleBan = () => {
-    console.log(`Banning user: ${props.userData.username}`);
-    // updateActivationStatus(props.userData._id, false);
-    setIsBanned(true);
+    console.log(`Deactivating user: ${props.userData.username}`);
+    // Assuming isActive is the correct property to toggle
+    setLoading(true);
+    var newProf = props.userData;
+    newProf.isActive = false;
+
+    updateUserProfile(newProf).then(() => {
+      setLoading(false);
+      setIsBanned(true);
+    });
   };
 
   const handleActivate = () => {
     console.log(`Activating user: ${props.userData.username}`);
-    // updateActivationStatus(props.userData._id, true);
-    setIsBanned(false);
+    // Assuming isActive is the correct property to toggle
+    setLoading(true);
+    var newProf = props.userData;
+    newProf.isActive = true;
+
+    updateUserProfile(newProf).then(() => {
+      setLoading(false);
+      setIsBanned(false);
+    });
   };
 
+  
+
+  const spinner = loading ? <Spinner /> : <></>;
+
   return (
-    <Card className="my-2" style={{width: "80%", margin: "auto"}}>
+    <Card className="my-2" style={{ width: "80%", margin: "auto" }}>
       <Card.Body
         style={{
           backgroundColor: "#0C0D34",
@@ -66,9 +93,7 @@ export default function Customer(props) {
           View Profile
         </Button>
         <Button className="button">View Maps</Button>
-        {/* <Button variant="danger" className="redButton">
-          Ban
-        </Button> */}
+
         <Dropdown className="mr-2">
           <Dropdown.Toggle
             variant="danger"
@@ -82,32 +107,38 @@ export default function Customer(props) {
             <Dropdown.Item
               onClick={handleDelete}
               style={{ width: "fit-content" }}
-              className="redButton mb-2 rounded-3"
+              className="redButton mb-2 rounded-3 text-white"
             >
               Delete
             </Dropdown.Item>
-            {isBanned ? (
+            {!props.userData.isActive ? (
               <Dropdown.Item
                 onClick={handleActivate}
-                style={{
-                  width: "fit-content",
-                  // textAlign: "center",
-                }}
-                className="bg-success ms-3 text-white rounded-2"
+                style={{ width: "fit-content", textAlign: "center" }}
+                className="bg-success text-white rounded-2 ms-3 text-white"
               >
                 Activate
               </Dropdown.Item>
             ) : (
               <Dropdown.Item
                 onClick={handleBan}
-                className="redButton rounded-2"
+                className="redButton rounded-2 text-white"
                 style={{ width: "fit-content" }}
               >
                 Deactivate
               </Dropdown.Item>
             )}
+            <Dropdown.Item
+              // onClick={handleToggleAdmin}
+              style={{ width: "fit-content" }}
+              className="redButton rounded-2 mt-2"
+            >
+              {!admin ? "Make Admin" : "Revoke Admin"}
+            </Dropdown.Item>
           </Dropdown.Menu>
         </Dropdown>
+
+        {spinner}
       </Container>
       <ConfirmationDialog
         show={showConfirmation}
